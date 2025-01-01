9a0bc52314fb624ae00c7610f656d53a02e475c6
belli <bellinna.uong@outlook.com>
2025-01-01 03:35:12 +0100

    basic constructor (FirstName LastName and Year)

---
+++ src/Student.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Student  {
+    private String firstName;
+    private String lastName;
+    private int gradeYear;
+    private int studentID;
+    private String courses;
+    private int tuitionBalance;
+    private int costOfCourse = 600;
+
+    //Constructor : prompt user to enter student's name and year
+    public Student(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter First Name: ");
+        this.firstName = in.nextLine();
+
+        System.out.println("Enter Last Name: ");
+        this.lastName = in.nextLine();
+
+        System.out.println("; - Freshmen\n2 - Sophomore\n3 - Junior\n4 - Senior\nEnter student class level :");
+        this.gradeYear = in.nextInt();
+        System.out.println(firstName + " " + lastName + " " + gradeYear);
+    }
+
+    // Generate an ID
+
+    // Enroll in courses
+
+    // View balance
+
+    // Pay the tuition
+
+    // Show status
+}
---
+++ src/StudentDatabaseApp.java
@@ -0,0 +1,8 @@
+public class StudentDatabaseApp {
+    public static void main(String[] args) {
+        Student stu1 = new Student();
+    // Ask how many new users we want to add
+
+        // Creat n number of new students
+    }
+}
